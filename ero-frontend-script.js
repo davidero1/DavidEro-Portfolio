8801588191ab984b7401e77328852febe0a537e0
David <daviderophillips@gmail.com>
2025-03-19 14:01:45 +0100

    responsiveness for mobile & tab

--- ero-frontend-script.js
+++ ero-frontend-script.js
@@ -1,3 +1,12 @@
+"use-strict";
+
+document.querySelector(".barsicon").addEventListener("click", function() {
+    const navBox = document.querySelector(".navbox");
+    const icon = document.querySelector(".fa-solid");
+    navBox.classList.toggle("show");
+    icon.classList.toggle("fa-xmark");
+});
+
 // slider
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
